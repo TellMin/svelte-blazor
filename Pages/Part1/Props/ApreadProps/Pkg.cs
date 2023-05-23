6d309593ffe46b5d96b92c6192e32c52bfb7cdad
TellMin <dhgirkcls@gmail.com>
2023-05-23 22:10:50 +0900

    Add: spread-props page

---
+++ Pages/Part1/Props/ApreadProps/Pkg.cs
@@ -0,0 +1,9 @@
+namespace svelte_blazor.Pages.Part1.Props.ApreadProps;
+
+public class Pkg
+{
+    public string? Name { get; set; }
+    public string? Speed { get; set; }
+    public int Version { get; set; }
+    public string? Website { get; set; }
+}
